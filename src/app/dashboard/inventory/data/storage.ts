f8e0f78c17a6cc7ff9ce066c7880d2c268a4bd81
YordyNeyraxyz <U22221179@utp.edu.pe>
2025-04-24 02:33:41 +0000

    completed data

---
+++ src/app/dashboard/inventory/data/storage.ts
@@ -0,0 +1,17 @@
+export const storage = [
+    {
+      id: 1,
+      name: "Kingston SSD NV2 500GB",
+      description: "Fast and reliable 500GB NVMe SSD.",
+      stock: 200,
+      price: 49,
+    },
+    {
+      id: 2,
+      name: "Samsung 970 EVO 1TB",
+      description: "1TB NVMe SSD with high-speed performance.",
+      stock: 95,
+      price: 129,
+    },
+  ];
+  
