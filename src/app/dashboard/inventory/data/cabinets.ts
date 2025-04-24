b253132ee715ec5682af682d26355fbc8e4bac80
YordyNeyraxyz <U22221179@utp.edu.pe>
2025-04-24 02:32:50 +0000

    added data

---
+++ src/app/dashboard/inventory/data/cabinets.ts
@@ -0,0 +1,17 @@
+export const cabinets = [
+    {
+      id: 1,
+      name: "NZXT H510 Elite",
+      description: "Mid Tower PC case with sleek design and RGB lighting.",
+      stock: 50,
+      price: 149,
+    },
+    {
+      id: 2,
+      name: "Corsair iCUE 4000X RGB",
+      description: "PC case with tempered glass panels and RGB fans.",
+      stock: 60,
+      price: 129,
+    },
+  ];
+  
