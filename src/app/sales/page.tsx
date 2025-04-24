10da83bb528f81316d6e931f7fb0c2a198de2806
KenyiPlatanazo <riositalo2@gmail.com>
2025-04-23 22:05:08 -0500

    modified:   package-lock.json 	modified:   package.json 	new file:   src/app/sales/page.tsx

---
+++ src/app/sales/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+import SalesModal from "./components/SalesModal";
+
+export default function SalesTestModule(){
+    return(
+        <main>
+            <SalesModal></SalesModal>
+        </main>
+    )
+}
+
